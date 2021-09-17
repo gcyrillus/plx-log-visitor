74f75c0331836f8c071c2ab1320ac47b679d0485
Grib Cyr <gccyrillus@hotmail.com>
2021-09-17 18:14:41 +0200

    Add files via upload

---
+++ visitor/visitor.php
@@ -0,0 +1,51 @@
+<?php
+    if(!defined('PLX_ROOT')) {
+        die('oups!');
+    }
+
+    class visitor extends plxPlugin {
+        const HOOKS = array(
+			'AdminTopEndHead',
+            'AdminUsersTop',
+        );
+        const BEGIN_CODE = '<?php' . PHP_EOL;
+        const END_CODE = PHP_EOL . '?>';
+
+        public function __construct($default_lang) {
+            # appel du constructeur de la classe plxPlugin (obligatoire)
+            parent::__construct($default_lang);
+
+            # Ajoute des hooks
+            foreach(self::HOOKS as $hook) {
+                $this->addHook($hook, $hook);
+            }
+        }
+		
+		public function AdminTopEndHead() {
+            echo self::BEGIN_CODE;
+?>
+		    if(isset($_SESSION['profil']) and $_SESSION['profil'] == PROFIL_VISITOR) { header("location: /");}
+<?php
+            echo self::END_CODE;
+        }
+
+        public function AdminUsersTop() {
+            echo self::BEGIN_CODE;
+?>
+# Tableau des profils
+$aProfils = array(
+	PROFIL_ADMIN => L_PROFIL_ADMIN,
+	PROFIL_MANAGER => L_PROFIL_MANAGER,
+	PROFIL_MODERATOR => L_PROFIL_MODERATOR,
+	PROFIL_EDITOR => L_PROFIL_EDITOR,
+	PROFIL_WRITER => L_PROFIL_WRITER,
+	PROFIL_VISITOR => L_PROFIL_VISITOR
+);
+
+<?php
+            echo self::END_CODE;
+			
+			
+        } 
+
+    }
